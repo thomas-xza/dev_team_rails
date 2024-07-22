0348c76c603e15f7638af7d5f17a722055ed83d8
Thomas <122806804+thomas-xza@users.noreply.github.com>
2024-07-22 15:10:32 +0000

    AH it is getting fiddly. THe guide encourages Rails-syntax for associating the comments to the project, but the syntax to make it work is very Rails-specific quirks.

--- rails_app/app/controllers/comments_controller.rb
+++ rails_app/app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @project = Project.find(params[:project_id])
+    @comment = @project.comments.create(comment_params)
+    redirect_to project_path(@project)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
 end
