6c6407162cacb3cf7ee73d323e5ecb31041d15d6
Thomas <122806804+thomas-xza@users.noreply.github.com>
2024-07-22 13:41:06 +0000

    Setup model for project, and a couple views for project selection and individual project

--- rails_app/app/controllers/projects_controller.rb
+++ rails_app/app/controllers/projects_controller.rb
@@ -1,4 +1,14 @@
 class ProjectsController < ApplicationController
+
+  
   def index
+    @projects = Project.all
   end
+
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  
 end
--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "projects#index"
 
   get "/projects", to: "projects#index"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/projects/:id", to: "projects#view"
+
 end
