149008b4c1b9304b8b78627a890e4b088ff86d3d
Thomas <122806804+thomas-xza@users.noreply.github.com>
2024-07-22 15:49:15 +0000

    Major wrangle to find out why the guide works and why my setup does not work despite the similarities. Conceiving the concept was definitely more fun than grinding through the implementation details. It might be easier to build in a way that you can understand how it works, rather than duplicating all the Rails quirks from the guide.

--- rails_app/app/controllers/comments_controller.rb
+++ rails_app/app/controllers/comments_controller.rb
@@ -1,14 +1,24 @@
 class CommentsController < ApplicationController
 
+  
   def create
+    
     @project = Project.find(params[:project_id])
+    
     @comment = @project.comments.create(comment_params)
+    
     redirect_to project_path(@project)
+    
   end
 
+  
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
-
+  
+  def comment_params
+    
+    params.require(:comment).permit(:commenter, :body)
+    
+  end
+  
+  
 end
