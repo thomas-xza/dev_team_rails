9b6ba32ceba13132f883cb317d9816ec5c9f0b76
Thomas <122806804+thomas-xza@users.noreply.github.com>
2024-07-22 14:31:44 +0000

    Hitting some kind of bug . Record appears in database, but is not seemingly accessible from view

--- rails_app/app/controllers/projects_controller.rb
+++ rails_app/app/controllers/projects_controller.rb
@@ -2,12 +2,16 @@ class ProjectsController < ApplicationController
 
   
   def index
+    
     @projects = Project.all
+    
   end
 
   
   def show
+    
     @project = Project.find(params[:id])
+    
   end
 
   
