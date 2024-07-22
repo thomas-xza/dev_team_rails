ecbff2f5dde58b24fa721e96ad5543d256da9247
Thomas <122806804+thomas-xza@users.noreply.github.com>
2024-07-22 18:02:24 +0000

    .erb commenting is not pretty so committing progress with conversation history before deleting. The logic for ordering the data is there, just wrangling with the syntax...

--- rails_app/app/controllers/projects_controller.rb
+++ rails_app/app/controllers/projects_controller.rb
@@ -11,6 +11,18 @@ class ProjectsController < ApplicationController
   def show
     
     @project = Project.find(params[:id])
+
+    @status_data = @project.statuses.as_json.
+                    map { |data| data.to_h }.
+                    map { |data| data.merge(type: "status") }
+
+    comment_data = @project.comments.as_json.
+                     map { |data| data.to_h }.
+                     map { |data| data.merge(type: "comment") }
+
+    all_data = @status_data + comment_data
+
+    @sorted_all_data = all_data.sort_by { |data| data["created_at"] }
     
   end
 
